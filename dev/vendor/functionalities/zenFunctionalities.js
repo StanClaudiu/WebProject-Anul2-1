a36bef66adf09eee0d948c3c8a7fef1b1e17da40
Radu <radustefan1302@gmail.com>
2022-05-23 01:45:08 +0300

    done jwt

--- dev/vendor/functionalities/zenFunctionalities.js
+++ dev/vendor/functionalities/zenFunctionalities.js
@@ -1,18 +1,51 @@
+import { User } from "../../models/index.js"
+import jwt from 'jsonwebtoken';
+
+const setDB = (zen, db) => {
+    if (!db) {
+        console.log("Invalid DB")
+    }
+    zen.db = db;
+    return zen
+}
+
+const addSession = async (zen, request) => {
+    let isLoggedIn = false
+    let user = null
+
+    do {
+        if (!request.cookies["authToken"]){
+            break
+        }
+
+        const authToken = request.cookies["authToken"]
+
+        try {
+            const decodedObject = jwt.verify(authToken, process.env.SECURITY_SECRET_KEY);
+            user = await User.getUserByEmail(zen.db, decodedObject["email"])
+
+            if (user != null) {
+                isLoggedIn = true
+            }
+        } 
+        catch(error) {
+            console.log(error)
+        }        
+
+    } while(false)
+
+    zen.session = {
+        "isLoggedIn": isLoggedIn,
+        "user": user
+    }
+    return zen
+}
+
 const AddZenFunctionalities = (zen) => {
     zen.augment = async (db, request) => {
-        if (!db) {
-            console.log("Invalid DB")
-        }
-        zen.db = db;
-
-        console.log(request.cookies)
-
-        zen.session = {
-            "isLoggedIn": true,
-            "user": null
-        }
+        zen = setDB(zen, db)
+        zen = addSession(zen, request)
     }
-    
     return zen
 }
 
