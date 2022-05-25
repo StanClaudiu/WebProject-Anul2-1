07cf09847a83f96a14914298a223de9b8984fe52
Radu <radustefan1302@gmail.com>
2022-05-25 21:18:29 +0300

    done

--- dev/vendor/database/oracle/plsql/PlantPackege.sql
+++ dev/vendor/database/oracle/plsql/PlantPackege.sql
@@ -30,18 +30,19 @@ CREATE SEQUENCE plant_seq START WITH 1 /* STATEMENT */;
 
 
 CREATE OR REPLACE PACKAGE plant_packege IS
-    TYPE table_type is table of base_user%ROWTYPE;
+    TYPE table_type is table of plant%ROWTYPE;
     
-    FUNCTION add_user ( user_role base_user.role%TYPE, 
-                        user_name base_user.name%TYPE,
-                        user_email base_user.email%TYPE,
-                        user_password base_user.password%TYPE) RETURN INT;
+    FUNCTION add_user ( id_user plant.id_user%TYPE, 
+                        id_type plant.id_type%TYPE,
+                        name plant.name%TYPE) RETURN INT;
                         
-    FUNCTION get_users RETURN table_type PIPELINED;          
+    FUNCTION get_plants RETURN table_type PIPELINED;          
     
-    FUNCTION get_user_by_id ( user_id base_user.id%type) RETURN table_type PIPELINED;
+    FUNCTION get_plant_by_id ( id_plant plant.id%TYPE) RETURN table_type PIPELINED;
     
-    FUNCTION get_user_by_email ( user_email base_user.email%type) RETURN table_type PIPELINED;
+    FUNCTION get_plants_by_user_id ( id_user plant.id_user%TYPE) RETURN table_type PIPELINED;
+    
+    FUNCTION delete_plant_by_id ( id_plant plant.id%TYPE) RETURN INT;
 END plant_packege;/* STATEMENT */
 
 
@@ -49,54 +50,59 @@ END plant_packege;/* STATEMENT */
 
 CREATE OR REPLACE PACKAGE BODY plant_packege IS
 
-    FUNCTION add_user ( user_role base_user.role%type, 
-                        user_name base_user.name%type,
-                        user_email base_user.email%type,
-                        user_password base_user.password%type) RETURN INT AS
+    FUNCTION add_plant ( id_user plant.id_user%TYPE, 
+                        id_type plant.id_type%TYPE,
+                        name plant.name%TYPE) RETURN INT;
          
-        CURSOR same_email_users IS SELECT email FROM base_user WHERE email = user_email;
-        v_aux base_user.email%type;
-        v_current_user_id INT; 
+        v_current_pant_id INT; 
         PRAGMA AUTONOMOUS_TRANSACTION;
     BEGIN
-        OPEN same_email_users;
-        FETCH same_email_users INTO v_aux;      
         
-        IF (same_email_users%NOTFOUND = FALSE)
-        THEN
-            raise_application_error(-20001, 'Already used email');
-        END IF;
-        
-        INSERT INTO base_user (id, role, name, email, password) VALUES (base_user_seq.NEXTVAL, user_role, user_name, user_email, user_password); 
+        INSERT INTO plant (id, id_user, id_type, name) VALUES (plant_seq.NEXTVAL, id_user, id_type, name); 
         COMMIT;
         
-        SELECT base_user_seq.CURRVAL INTO v_current_user_id FROM DUAL;
-        RETURN v_current_user_id;
+        SELECT plant_seq.CURRVAL INTO v_current_pant_id FROM DUAL;
+        RETURN v_current_pant_id;
     END add_user;         
     
-    FUNCTION get_users RETURN table_type PIPELINED AS
-        CURSOR table_cursor IS SELECT * FROM base_user;
+    FUNCTION get_plants RETURN table_type PIPELINED AS
+        CURSOR table_cursor IS SELECT * FROM plant;
     BEGIN
         FOR current_record IN table_cursor LOOP
             PIPE ROW(current_record);
         END LOOP;
-    END get_users;          
+    END get_plants;          
     
-    FUNCTION get_user_by_id ( user_id base_user.id%TYPE) RETURN table_type PIPELINED AS
-        CURSOR table_cursor IS SELECT * FROM base_user WHERE id = user_id; 
+    FUNCTION get_plant_by_id ( id_plant plant.id%TYPE) RETURN table_type PIPELINED AS
+        CURSOR table_cursor IS SELECT * FROM plant WHERE id = id_plant; 
     BEGIN
         FOR current_record IN table_cursor LOOP
             PIPE ROW(current_record);
         END LOOP;
-    END get_user_by_id;   
+    END get_plant_by_id;   
     
-    FUNCTION get_user_by_email ( user_email base_user.email%TYPE) RETURN table_type PIPELINED AS
-        CURSOR table_cursor IS SELECT * FROM base_user WHERE email = user_email;
+    FUNCTION get_plants_by_user_id ( id_user plant.id_user%TYPE) RETURN table_type PIPELINED AS
+        CURSOR table_cursor IS SELECT * FROM plant WHERE id_user = id_user;
     BEGIN
         FOR current_record IN table_cursor LOOP
             PIPE ROW(current_record);
         END LOOP;
     END get_user_by_email;  
+    
+        FUNCTION add_plant ( id_user plant.id_user%TYPE, 
+                        id_type plant.id_type%TYPE,
+                        name plant.name%TYPE) RETURN INT;
+         
+        v_current_pant_id INT; 
+        PRAGMA AUTONOMOUS_TRANSACTION;
+    BEGIN
+        
+        INSERT INTO plant (id, id_user, id_type, name) VALUES (plant_seq.NEXTVAL, id_user, id_type, name); 
+        COMMIT;
+        
+        SELECT plant_seq.CURRVAL INTO v_current_pant_id FROM DUAL;
+        RETURN v_current_pant_id;
+    END add_user;   
     
 END plant_packege;/* STATEMENT */
 
