a84c488b7576ee7a4e015d429c9255436e2e0111
deliagrigorita <deliagrigorita@yahoo.com>
2022-05-27 22:50:30 +0300

    Starting seeding users

---
+++ dev/seder/index.js
@@ -0,0 +1,1 @@
+export { default as SeedAdmins } from "./adminSeder.js"
--- dev/zen.js
+++ dev/zen.js
@@ -1,4 +1,5 @@
 import { OracleDatabase } from "./vendor/database/index.js";
+import { SeedAdmins } from "./seder/index.js"
 import { App } from "./vendor/index.js";
 import { mainRoutes } from "./routes/web/index.js";
 import { authRoutes } from "./routes/api/index.js";
@@ -9,6 +10,10 @@ await db.connect()
 
 if (parseInt(process.env.DB_INIT_ENVIRONMENT) == 1) {
     await db.initEnvironment();
+}
+
+if (parseInt(process.env.DB_SEED) == 1) {
+    await SeedAdmins(db)
 }
 
 const app = new App(process.env.PORT || 4000, db)
