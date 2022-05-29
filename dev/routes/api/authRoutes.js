5e9e39251505588727b82039e8ab2b3ed8b608d0
Radu <radustefan1302@gmail.com>
2022-05-29 12:43:49 +0300

    admin page

--- dev/routes/api/authRoutes.js
+++ dev/routes/api/authRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from "../../vendor/index.js";
-import { AuthController } from "../../controllers/index.js";
+import { AuthController, AdminPageController } from "../../controllers/index.js";
 
 const authRoutes = new Router();
 
 authRoutes.post(`/api/v1/register`, AuthController.register);
 authRoutes.post(`/api/v1/login`, AuthController.login);
 authRoutes.get(`/api/v1/logout`, AuthController.logout);
+
+authRoutes.post(`/api/v1/courses`, AdminPageController.create);
 
 export default authRoutes
