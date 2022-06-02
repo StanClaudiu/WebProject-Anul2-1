9bc3ad63e722df95d7ee10e3a253d09c75327cab
Radu <radustefan1302@gmail.com>
2022-06-02 22:04:32 +0300

    almost done courses

--- dev/controllers/coursePageController.js
+++ dev/controllers/coursePageController.js
@@ -1,12 +1,26 @@
 import { StatusCodes } from "http-status-codes"
 import { Course } from "../models/index.js"
 
+const buildCoursesTree = async (startingCourse) => {
+    let rootCourse = {}
+
+    if (startingCourse.parentCourseId == null) {
+        rootCourse = startingCourse
+    }
+    else {
+        rootCourse = await Course.getById(rootCourse.parentCourseId)
+    }
+
+    rootCourse.childCourses = await rootCourse.getChildCourses()
+    return rootCourse
+}
+
 const CoursePageController = {
 
     view: async (zen, request, response) => {
         const course = await Course.getById(zen.db, request.parameters.id)
         await response.sendZenView(
-            {"course": course}, "views/course.html")
+            {"course": course, "courseTree": await buildCoursesTree(course)}, "views/course.html")
     }
 }
 
