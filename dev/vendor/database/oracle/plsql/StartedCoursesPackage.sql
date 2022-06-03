ef3f3fbbe05e22137620701aaa8e46866c1bdf60
Radu <radustefan1302@gmail.com>
2022-06-03 10:01:14 +0300

    started courses repository

--- dev/vendor/database/oracle/plsql/StartedCoursesPackage.sql
+++ dev/vendor/database/oracle/plsql/StartedCoursesPackage.sql
@@ -26,68 +26,81 @@ CREATE SEQUENCE started_courses_seq START WITH 1/* STATEMENT */;
 
 -----let's make the package
 
-    CREATE OR REPLACE PACKAGE started_courses_package IS
-        
-        CURSOR table_for_return_allCourses IS SELECT sc.id_statistics , sc.id_curs, sc.progress,c.course_name,c.description_course   FROM started_courses sc RIGHT JOIN courses c on sc.id_curs=c.id_curs; 
-        ----this is just for the type
-        
-        TYPE preview_courses_table IS TABLE OF table_for_return_allCourses%ROWTYPE;
-        
-        FUNCTION create_started_course (p_id_user started_courses.id_user%TYPE,
-                                        p_id_curs started_courses.id_curs%TYPE) RETURN INT ;
-        FUNCTION update_started_course (id_stat started_courses.id_curs%TYPE,
-                                        p_progress started_courses.id_curs%TYPE) RETURN INT;
-        FUNCTION getAllStartedCourses(p_id_user started_courses.id_user%TYPE) 
-                                                                        RETURN preview_courses_table PIPELINED;
-        
-    END started_courses_package;/* STATEMENT */
+CREATE OR REPLACE PACKAGE started_courses_package IS
     
-   
-         
+    CURSOR table_for_return_allCourses IS SELECT sc.id_statistics , sc.id_curs, sc.progress,c.course_name,c.description_course   FROM started_courses sc RIGHT JOIN courses c on sc.id_curs=c.id_curs; 
+    ----this is just for the type
     
-    CREATE OR REPLACE PACKAGE BODY started_courses_package IS
-        
-        
-         FUNCTION create_started_course (p_id_user started_courses.id_user%TYPE,
-                                        p_id_curs started_courses.id_curs%TYPE) RETURN INT AS      
-            PRAGMA AUTONOMOUS_TRANSACTION;
-            id_val_stat INT;    
-         BEGIN
-            INSERT INTO started_courses VALUES(started_courses_seq.NEXTVAL,p_id_user,p_id_curs,0);
-            COMMIT;
-            SELECT started_courses_seq.CURRVAL INTO id_val_stat FROM DUAL;
-            RETURN id_val_stat;
-         EXCEPTION 
-            WHEN others THEN
-                RAISE_APPLICATION_ERROR(-20000, 'ERROR IN INSERTING A NEW COURSE PROGRESS IN DB!');
-                RETURN -1;
-         END create_started_course;
+    TYPE preview_courses_table IS TABLE OF table_for_return_allCourses%ROWTYPE;
+    
+    FUNCTION create_started_course (p_id_user started_courses.id_user%TYPE,
+                                    p_id_curs started_courses.id_curs%TYPE) RETURN INT ;
+    FUNCTION update_started_course (id_stat started_courses.id_curs%TYPE,
+                                    p_progress started_courses.id_curs%TYPE) RETURN INT;
+    FUNCTION getAllStartedCourses(p_id_user started_courses.id_user%TYPE) 
+                                                                    RETURN preview_courses_table PIPELINED;          
+    FUNCTION getByUserAndCourse(p_id_user started_courses.id_user%TYPE,
+                                p_id_course started_course.id_curs%TYPE) 
+                                                                    RETURN preview_courses_table PIPELINED;
+    
+END started_courses_package;/* STATEMENT */
+
          
-         FUNCTION update_started_course (id_stat started_courses.id_curs%TYPE,
-                                        p_progress started_courses.id_curs%TYPE) RETURN INT IS
-            PRAGMA AUTONOMOUS_TRANSACTION;
-        BEGIN
-            IF(p_progress>=0 AND p_progress<=1)THEN
-            UPDATE started_courses  SET progress=p_progress WHERE started_courses.id_statistics = id_stat; 
-            COMMIT;
-            RETURN 1;
-            ELSE
-                    RAISE_APPLICATION_ERROR(-20002,'Something is not good,give me to the catcher');
+
+CREATE OR REPLACE PACKAGE BODY started_courses_package IS
+    
+    
+     FUNCTION create_started_course (p_id_user started_courses.id_user%TYPE,
+                                    p_id_curs started_courses.id_curs%TYPE) RETURN INT AS      
+        PRAGMA AUTONOMOUS_TRANSACTION;
+        id_val_stat INT;    
+     BEGIN
+        INSERT INTO started_courses VALUES(started_courses_seq.NEXTVAL,p_id_user,p_id_curs,0);
+        COMMIT;
+        SELECT started_courses_seq.CURRVAL INTO id_val_stat FROM DUAL;
+        RETURN id_val_stat;
+     EXCEPTION 
+        WHEN others THEN
+            RAISE_APPLICATION_ERROR(-20000, 'ERROR IN INSERTING A NEW COURSE PROGRESS IN DB!');
             RETURN -1;
-            END IF;
-            
-        EXCEPTION WHEN others THEN
-        RAISE_APPLICATION_ERROR(-20001,'ERROR UPDATING A STARTED COURSE PROGRESS');
-            RETURN -1;
-        END update_started_course;
+     END create_started_course;
+     
+     FUNCTION update_started_course (id_stat started_courses.id_curs%TYPE,
+                                    p_progress started_courses.id_curs%TYPE) RETURN INT IS
+        PRAGMA AUTONOMOUS_TRANSACTION;
+    BEGIN
+        IF(p_progress>=0 AND p_progress<=1)THEN
+        UPDATE started_courses  SET progress=p_progress WHERE started_courses.id_statistics = id_stat; 
+        COMMIT;
+        RETURN 1;
+        ELSE
+                RAISE_APPLICATION_ERROR(-20002,'Something is not good,give me to the catcher');
+        RETURN -1;
+        END IF;
         
-        FUNCTION getAllStartedCourses(p_id_user started_courses.id_user%TYPE) RETURN preview_courses_table PIPELINED IS
-            CURSOR table_started_courses IS SELECT sc.id_statistics , c.id_curs, sc.progress,c.course_name,c.description_course  
-            FROM started_courses sc RIGHT JOIN courses c on sc.id_curs=c.id_curs where id_user=p_id_user OR (NVL(id_user,-1)=-1 AND NVL(c.parent_id,-1)=-1);
-        BEGIN
-            FOR current_course IN table_started_courses LOOP
-                PIPE ROW(current_course);
-            END LOOP;
-        END;
-        
-  END started_courses_package;/* STATEMENT */
+    EXCEPTION WHEN others THEN
+    RAISE_APPLICATION_ERROR(-20001,'ERROR UPDATING A STARTED COURSE PROGRESS');
+        RETURN -1;
+    END update_started_course;
+    
+    FUNCTION getAllStartedCourses(p_id_user started_courses.id_user%TYPE) RETURN preview_courses_table PIPELINED IS
+        CURSOR table_started_courses IS SELECT sc.id_statistics , c.id_curs, sc.progress,c.course_name,c.description_course  
+        FROM started_courses sc RIGHT JOIN courses c on sc.id_curs=c.id_curs where id_user=p_id_user OR (NVL(id_user,-1)=-1 AND NVL(c.parent_id,-1)=-1);
+    BEGIN
+        FOR current_course IN table_started_courses LOOP
+            PIPE ROW(current_course);
+        END LOOP;
+    END;
+    
+    FUNCTION getByUserAndCourse(p_id_user started_courses.id_user%TYPE,
+                                p_id_course started_course.id_curs%TYPE) RETURN preview_courses_table PIPELINED IS
+                                
+        CURSOR table_cursor IS SELECT sc.id_statistics , c.id_curs, sc.progress  
+        FROM started_courses sc RIGHT JOIN courses c on sc.id_curs=c.id_curs where id_user=p_id_user AND id_curs=p_id_course;
+    BEGIN
+        FOR current_course IN table_started_courses LOOP
+            PIPE ROW(current_course);
+        END LOOP;
+    END;
+    
+END started_courses_package;/* STATEMENT */
--- dev/vendor/database/oracle/repositories/StartedCourseRepository.js
+++ dev/vendor/database/oracle/repositories/StartedCourseRepository.js
@@ -30,6 +30,58 @@ class StartedCourseUserRepository {
             console.error(err);
         }
     }
+
+    async getAllStratedCourses (userId) {
+        try {
+            const result = await this.db.execute(
+                `SELECT * from TABLE( started_courses_package.getAllStartedCourses('${userId}'))`);
+            
+            console.log(result);
+            return result.rows; //the id
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    async create(user_id, course_id) {
+        try {
+            const result = await this.db.execute(
+                `SELECT started_courses_package.create_started_course('${user_id}', '${course_id}') FROM DUAL`);
+            
+            console.log(result);
+            return result.rows[0][Object.keys(result.rows[0])[0]]; //the id
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    async update(id, progress) {
+        try {
+            const result = await this.db.execute(
+                `SELECT started_courses_package.update_course(${id}, ${progress}) FROM DUAL`);
+            
+            console.log(result);
+            return result.rows[0][Object.keys(result.rows[0])[0]]; //the id
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    async getByUserAndCourse(userId, courseId) {
+        try {
+            const result = await this.db.execute(
+                `SELECT started_courses_package.getByUserAndCourse(${userId}, ${courseId}) FROM DUAL`);
+            
+            console.log(result);
+            return result.rows.length == 0 ? null : result.rows[0]; //the id
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
 }
 
 export default StartedCourseUserRepository;
