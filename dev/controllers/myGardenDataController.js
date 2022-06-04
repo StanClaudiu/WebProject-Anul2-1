41a07acf7b0b3463aa07bb2ec98f3782f72c19a3
Radu <radustefan1302@gmail.com>
2022-06-04 09:20:24 +0300

    done upload files

--- dev/controllers/myGardenDataController.js
+++ dev/controllers/myGardenDataController.js
@@ -1,5 +1,15 @@
 import { StatusCodes } from "http-status-codes"
 import { Plant, PlantType, Reminder } from "../models/index.js"
+import fs from "fs"
+
+const isIsInPlantTypes = (id, plantTypes) => {
+    for (let it = 0; it < plantTypes.length; it++) {
+        if (plantTypes[it].id == id) {
+            return true;
+        }
+    }
+    return false;
+}
 
 const MyGardenDataController = {
 
@@ -22,18 +32,31 @@ const MyGardenDataController = {
     },
 
     upload: async (zen, request, response) => {
+        const uploadedFile = request.body.files["file_app"]
+        const uploadedData = fs.readFileSync(uploadedFile.filepath)
 
-        let userPlantTypes = await PlantType.getUserPlantTypes(zen.db,zen.session.user.id);
-        let plantTypes = await PlantType.getPlantTypes(zen.db);
-        console.log(plantTypes);
-     
-        let reminders = await Reminder.getByUserId(zen.db,zen.session.user.id);
-        console.log(reminders);
-        
-     
-        response.sendZenView({"plantTypes": plantTypes , "userPlantTypes" : userPlantTypes,"reminders":reminders},"views/myGarden.html");
-        
-        return response;
+        const plantTypes = await PlantType.getPlantTypes(zen.db);
+
+        const dictData = JSON.parse(uploadedData)
+
+        for (let it = 0; it < dictData.length; it++) {
+            if (isIsInPlantTypes(dictData[it].id, plantTypes)) {
+
+                for (let jt = 0; jt < dictData[it]["plantsOfType"].length; jt++) {
+
+                    const plantData = dictData[it]["plantsOfType"][jt]
+
+                    const plant = new Plant(zen.db, 
+                        plantData["id_plant_type"],
+                        plantData["id_user"],
+                        plantData["plant_name"] )
+                    await plant.create()
+
+                }
+            }
+        }
+
+        response.redirect("/myGarden")
     }
 }
 
